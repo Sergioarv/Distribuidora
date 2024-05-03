8f04b3430277826378ecac6fce765e732ab7cfe1
Sergio Rodriguez Vasquez <sarv9208@hotmail.com>
2024-05-02 20:18:45 -0500

    Creación de repositorio de proyecto

---
+++ src/test/java/co/com/sergio/distribuidora/distribuidora/DistribuidoraApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sergio.distribuidora.distribuidora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
